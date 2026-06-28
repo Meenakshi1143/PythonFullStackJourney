dcb085092a92bb0738fb59009645b71203de5923
Meenakshi Viyyapu <meenav1143@gmail.com>
2026-06-28 15:48:07 +0530

    Update Day1 Introduction.py
    This file covers the fundamentals of Python, including an introduction to procedural and object-oriented programming, key features of Python, reasons for using Python, tokens, variable naming rules, comments, Boolean values, and basic examples demonstrating Python syntax and execution.

--- Day1 Introduction.py
+++ Day1 Introduction.py
@@ -1,22 +1,20 @@
-Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 22 June 2026
 
 '''
 Procedural Language
 -------------------
---> This follows step-by-step approach, where code is structure into procedures such as function or routines...
+--> This follows step-by-step approach, where code is structure into procedures such as function or routines
 	Eg: C
 
 Object Oriented
 ---------------
--->This is based on concept of object and classes...
+-->This is based on concept of object and classes
 	Eg: python, Java
 
 Python(1991, Guide Van Rossum)
 ------
 1. Dynamically typed language
---> Python knows the type of data we are passing to the variable...
+--> Python knows the type of data we are passing to the variable
 
 2. Interpreter language
 --> Python execute code line-by-line, if any error occur it will stop execution in that line and before lines will gives the ooutput.
@@ -28,7 +26,7 @@ Why use python
 --------------
 --> Easy syntax
 --> Cross-platform
---> Wide applications(AI, Ml, Dl,.....)
+--> Wide applications(AI, Ml, Dl,..)
 --> Huge library support
 
 
@@ -37,19 +35,19 @@ Tokens
 
 1. Keywords
 	These are reserved words in python 
-	Eg:if, else, for, while,....
+	Eg:if, else, for, while,.
 
 2. Identifiers
-	Name given to variables, functions, class,...
+	Name given to variables, functions, class,
 
 3. Literals
-	89, "Halloo", 4.46,...
+	89, "Halloo", 4.46,
 
 4. Operators
-	+, -, *, /,...
+	+, -, *, /,
 
 5. Punctuations
-	(), {}, [],...
+	(), {}, [],
 
 Rules of variables
 ------------------
@@ -61,47 +59,38 @@ Rules of variables
 --> Do not way to start with Special characters, space, numbers
 	Eg: @num = 0
 	    n*umm = 12
-... 	    1num = 85
-... 	    nu12m = 8332
-... 	    n u m = 809832
-... 	    
-... Comments
-... --------
-... 
-... Single line comments --> #
-... 	Eg: num = 9
-... 	    if num % 2 == 0:  #checking a number is even or odd
-... 	    	print("Even")	
-...             else:
-...             	print("Odd")
-... 2. Multi line comments --> '''___''', """___"""
-... 
-... 
-... Boolean
-... -------
-... 1. True
-... 2. False
-... Eg:
-...     num = 89
-...     num_2 = 89
-...     print(num == num_2)
-... 
-... '''
-... 
-... 
-... print(89 + 67)
-... num = 89
-... num_2 = 67
-... total_ = num + num_2
-... print(id(num))
-... print(id(num_2))
-... print(id(total_))
-SyntaxError: invalid syntax
->>> 
->>> print(89 + 67)
-156
->>> print(id(num))
-Traceback (most recent call last):
-  File "<pyshell#3>", line 1, in <module>
-    print(id(num))
-NameError: name 'num' is not defined. Did you mean: 'sum'?
+ 	    1num = 85
+ 	    nu12m = 8332
+ 	    n u m = 809832
+ 	    
+ Comments
+ --------
+ 
+ Single line comments --> #
+ 	Eg: num = 9
+ 	    if num % 2 == 0:  #checking a number is even or odd
+ 	    	print("Even")	
+             else:
+             	print("Odd")
+ 2. Multi line comments --> '''___''', """___"""
+ 
+ 
+ Boolean
+ -------
+ 1. True
+ 2. False
+ Eg:
+     num = 89
+     num_2 = 89
+     print(num == num_2)
+ 
+ '''
+print(89 + 67)
+num = 89
+num_2 = 67
+total_ = num + num_2
+print(id(num))
+print(id(num_2))
+print(id(total_))
+print(89 + 67)
+print(id(num))
